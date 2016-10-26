fb2adc0b2ac88ab6dcfb3e9f5b51bdbe97e43831
Lee Chang <leechang@ntdev.microsoft.com>
2016-10-26 12:28:28 -0700

    UT WIP

--- test/parseTest.js
+++ test/parseTest.js
@@ -1,3 +1,60 @@
-define(["require", "./sampleManifest", "lib/apkreader/parser/manifest"], function(require, sampleManifest, manifest){
+define(["require", "node_modules/q/q", "./sampleManifest", "lib/apkreader/parser/manifest"], function (require, q, sampleManifest, manifestParser) {
     chai.should();
+
+    describe("Requiring", function () {
+        it("should find the manifest parser", function () {
+            manifestParser.should.not.be.null;
+        });
+    });
+
+    describe("APK Parser", function () {
+        var blob = dataURItoBlob(sampleManifest);
+
+        it("should correctly parse a binary androidManifest.xml", function (done) {
+
+            blobToArrayBuffer(blob).then(function (arrayBuffer) {
+
+                arrayBuffer.should.not.be.null;
+                var parser = new manifestParser(arrayBuffer);
+                var manifest = parser.parse();
+                console.log(manifest);
+            }).then(function () {
+                done();
+            }).catch(function(error){
+                done(error);
+            });
+        })
+    });
+
+    function dataURItoBlob(dataURI) {
+        var byteString = atob(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+        // write the bytes of the string to an ArrayBuffer
+        var ab = new ArrayBuffer(byteString.length);
+        var ia = new Uint8Array(ab);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        // write the ArrayBuffer to a blob, and you're done
+        var blob = new Blob([ab], { type: mimeString });
+        return blob;
+    }
+
+    function blobToArrayBuffer(blob) {
+        var deferred = q.defer();
+        var arrayBuffer;
+        var fileReader = new FileReader();
+
+        fileReader.onload = function () {
+            arrayBuffer = this.result;
+            deferred.resolve(arrayBuffer);
+        };
+
+        fileReader.readAsArrayBuffer(blob);
+        return deferred.promise;
+    }
 });
